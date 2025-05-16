44d7c6f28202a8a80241ed133a818c71dd7e2753
Administrator <Administrator@B508PC29>
2025-05-16 14:27:35 +0700

    Add project files.

---
+++ Nhom14/NhanVien.cs
@@ -0,0 +1,18 @@
+﻿namespace Phan1_QuanLyNhanVien
+{
+    public class NhanVien
+    {
+        public string MaSo { get; set; }
+        public string HoTen { get; set; }
+        public string NgaySinh { get; set; }
+        public string Email { get; set; }
+
+        public NhanVien(string maSo, string hoTen, string ngaySinh, string email)
+        {
+            MaSo = maSo;
+            HoTen = hoTen;
+            NgaySinh = ngaySinh;
+            Email = email;
+        }
+    }
+}
---
+++ Nhom14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Phan1_QuanLyNhanVien
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
